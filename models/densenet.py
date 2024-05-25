e2c7dab5098291cf6c62378f8d87da66610c244f
齐育栋 <120462130+Quiyyy@users.noreply.github.com>
2024-05-26 03:20:17 +0800

    Update densenet.py
    使用深度可分离卷积

--- models/densenet.py
+++ models/densenet.py
@@ -4,67 +4,66 @@ import torch.nn as nn
 import torch.nn.functional as F
 from thop import profile
 
+# Depthwise Separable Convolution
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, use_dropout=False):
+        super(DepthwiseSeparableConv, self)。__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=padding, groups=in_channels, bias=False)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(p=0.2)
 
-# DenseNet-B
+    def forward(self, x):
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        out = self.bn(out)
+        out = F.relu(out, inplace=True)
+        if self.use_dropout:
+            out = self.dropout(out)
+        return out
+
+# Bottleneck with Depthwise Separable Convolution
 class Bottleneck(nn.Module):
     def __init__(self, nChannels, growthRate, use_dropout):
-        super(Bottleneck, self).__init__()
+        super(Bottleneck, self)。__init__()
         interChannels = 4 * growthRate
-        self.bn1 = nn.BatchNorm2d(interChannels)
-        self.conv1 = nn.Conv2d(nChannels, interChannels, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(growthRate)
-        self.conv2 = nn.Conv2d(interChannels, growthRate, kernel_size=3, padding=1, bias=False)
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
+        self.conv1 = DepthwiseSeparableConv(nChannels, interChannels, kernel_size=1, padding=0, use_dropout=use_dropout)
+        self.conv2 = DepthwiseSeparableConv(interChannels, growthRate, kernel_size=3, padding=1, use_dropout=use_dropout)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = F.relu(self.bn2(self.conv2(out)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = torch.cat((x, out), 1)
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = torch.cat((x, out)， 1)
         return out
 
-
-# single layer
+# SingleLayer with Depthwise Separable Convolution
 class SingleLayer(nn.Module):
     def __init__(self, nChannels, growthRate, use_dropout):
-        super(SingleLayer, self).__init__()
-        self.bn1 = nn.BatchNorm2d(nChannels)
-        self.conv1 = nn.Conv2d(nChannels, growthRate, kernel_size=3, padding=1, bias=False)
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
+        super(SingleLayer, self)。__init__()
+        self.conv1 = DepthwiseSeparableConv(nChannels, growthRate, kernel_size=3, padding=1, use_dropout=use_dropout)
 
     def forward(self, x):
-        out = self.conv1(F.relu(x, inplace=True))
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = torch.cat((x, out), 1)
+        out = self.conv1(x)
+        out = torch.cat((x, out)， 1)
         return out
 
-
-# transition layer
+# Transition Layer
 class Transition(nn.Module):
     def __init__(self, nChannels, nOutChannels, use_dropout):
-        super(Transition, self).__init__()
-        self.bn1 = nn.BatchNorm2d(nOutChannels)
-        self.conv1 = nn.Conv2d(nChannels, nOutChannels, kernel_size=1, bias=False)
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
+        super(Transition, self)。__init__()
+        self.conv1 = DepthwiseSeparableConv(nChannels, nOutChannels, kernel_size=1, padding=0, use_dropout=use_dropout)
+        self.pool = nn.AvgPool2d(2, ceil_mode=True)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = F.avg_pool2d(out, 2, ceil_mode=True)
+        out = self.conv1(x)
+        out = self.pool(out)
         return out
 
-
+# DenseNet with Depthwise Separable Convolution
 class DenseNet(nn.Module):
     def __init__(self, params):
-        super(DenseNet, self).__init__()
+        super(DenseNet, self)。__init__()
         growthRate = params['densenet']['growthRate']
         reduction = params['densenet']['reduction']
         bottleneck = params['densenet']['bottleneck']
@@ -89,7 +88,7 @@ class DenseNet(nn.Module):
 
     def _make_dense(self, nChannels, growthRate, nDenseBlocks, bottleneck, use_dropout):
         layers = []
-        for i in range(int(nDenseBlocks)):
+        for i 在 range(int(nDenseBlocks)):
             if bottleneck:
                 layers.append(Bottleneck(nChannels, growthRate, use_dropout))
             else:
@@ -107,3 +106,4 @@ class DenseNet(nn.Module):
         out = self.trans2(out)
         out = self.dense3(out)
         return out
+
